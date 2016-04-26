40806d2e3a59c2bbcb416e8a96e4d9019d24ac14
Денис Галямин <galyamin.d.d@gmail.com>
2016-04-26 16:57:14 +0300

    lower case locals

--- remove_spaces.sh
+++ remove_spaces.sh
@@ -4,6 +4,6 @@ DIR="src/raw/pages/info/documents/locals"
 for f in $(ls $DIR); do
   #mv $DIR/$f/JPEG/*.jpg $DIR/$f;
   #rm -rf $DIR/$f/JPEG
-	#mv "$DIR/$f" $DIR/${f// /-}
-  echo "<li><a class=\"link-icon\" href=\"/pages/info/documents/locals/$f\">${f/-/--}</a></li>"
+	 mv "$DIR/$f" $(echo $DIR/$f| tr [:upper:] [:lower:])
+  #echo "<li><a class=\"link-icon\" href=\"/pages/info/documents/locals/$f\">${f/-/--}</a></li>"
 done
