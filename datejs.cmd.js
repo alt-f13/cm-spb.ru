9fba7a98c717bee7de6be7eaff436a3c2d9bc828
alt-f13 <galyamin.d.d@gmail.com>
2016-04-30 12:59:02 +0300

    конвертация стажа в дату начала стажа

--- datejs.cmd.js
+++ datejs.cmd.js
@@ -1,17 +1,24 @@
 #!/usr/local/bin/node
 require('datejs');
 var args = process.argv.slice(2);
-///console.log(args);
-years=args[0].slice(0, -1);
-months=args[1].slice(0, -1);
-day=args[2].slice(0, -1);
+//console.log(args);
+
+var arg = args[0].split(/ /);
+//console.log(arg);
+years=arg[0].slice(0, -1);
+months=arg[1].slice(0, -1);
 count=years*365;
-///console.log(count);
+
+if(arg[2]) {
+  day=arg[2].slice(0, -1);
+  count+=day*1;
+
+}
+//console.log(count);
 
 count+=months*30;
-///console.log(count);
+//console.log(count);
 
-count+=day*1;
 ///console.log(count);
 ///console.log(years+" "+months+" "+day);
 console.log(Date.today().add(-count).days().toString('dd/MM/yyyy')   );
--- parse_cvs.sh
+++ parse_cvs.sh
@@ -6,8 +6,10 @@ while read a b c d e f g h i j
  do
    DDIR=$DIR/$a
 mkdir -p $DDIR
-SINCE=$(node datejs.cmd.js $g)
-echo $SINCE
+echo ${g/./ }
+since=$(./datejs.cmd.js ${g/./ })
+edusince=$(./datejs.cmd.js ${h/./ })
+
 img=$(ls $DDIR|grep jpg)
 echo $img|wc
 echo -e "---\n\
@@ -19,13 +21,19 @@ fathername:\t $c\n\
 education:\t $d\n\
 category:\t $e\n\
 appointment:\t $f\n\
-since:\t $g\n\
-edusince:\t $i $h\n\
+since:\t $since\n\
 layout:\t faces" > $DDIR/index.html
 img_f=$DDIR/$img
+echo $since
+
 if [ -f $img_f ];then
   echo -e "img:\t /faces/$a/$img" >> $DDIR/index.html;
 fi
+if [ "$h" != " " ]; then
+echo -e "edusince:\t $edusince\n" >> $DDIR/index.html;
+echo $edusince
+fi
+
 echo -e "---" >> $DDIR/index.html
  done < $1
  IFS=$OLDIFS
