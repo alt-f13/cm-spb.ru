91ac677febf2bd8c9ec980ef7fd39d2b77069406
Денис Галямин <galyamin.d.d@gmail.com>
2016-06-06 13:33:58 +0300

    faces5 транслит; исправлены неточности

--- parse_cvs.sh
+++ parse_cvs.sh
@@ -5,17 +5,17 @@ IFS=";"
 while read a b c d e f g h i j
  do
 	 trans=$(echo $a$b|./bin/translit.py)
-   oDIR=$DIR/$a$b
-	 DDIR=$DIR/$trans
-	 if [ -d "$oDIR"]; then
+   oDIR=$DIR/$trans
+	 DDIR=$DIR/${trans/\'/}
+	 if [ -d "$oDIR" ]; then
 		 mv $oDIR $DDIR
 	 fi
 
 mkdir -p $DDIR/files
 echo ${h/./ } ${i/./ }
-since=$(./datejs.cmd.js ${h/./ })
-edusince=$(./datejs.cmd.js ${i/./ })
-echo $a $b $c $d $e $f $g $h $since $edusince
+since=$(./datejs.cmd.js ${f})
+edusince=$(./datejs.cmd.js ${g})
+echo $a $b $c $d $e $f $g $since $edusince
 
 img=$(ls $DDIR|grep jpg)
 echo $img|wc
@@ -27,14 +27,14 @@ firstname:\t $b\n\
 fathername:\t $c\n\
 education:\t $d\n\
 category:\t $e\n\
-appointment:\t $g\n\
+appointment:\t $h\n\
 since:\t $since\n\
 layout:\t faces" > $DDIR/index.html
 img_f=$DDIR/$img
 echo $since
 
 if [ -f $img_f ];then
-  echo -e "img:\t /faces/$a$b/$img" >> $DDIR/index.html;
+  echo -e "img:\t /faces/${trans/\'/}/$img" >> $DDIR/index.html;
 fi
 if [ "$i" != " " ]; then
 echo -e "edusince:\t $edusince\n" >> $DDIR/index.html;
