d07ce22577697f1357064935c8797b344f166272
Денис Галямин <galyamin.d.d@gmail.com>
2016-12-22 11:28:30 +0300

    новое расписание, вычещен персонал, поправлен Layot & index

--- src/raw/js/scheduler.js
+++ src/raw/js/scheduler.js
@@ -17,28 +17,13 @@ var $app = angular
     'ngHandsontable'
   ])
   .config(function ($httpProvider,couchConfigProvider) {
-      if(location.hostname === 'localhost') {
-        couchConfigProvider.setServer("/db");
-      }else {
-        couchConfigProvider.setServer("https://couch.2d-it.ru");
-      }
+		couchConfigProvider.setServer("https://couch.2d-it.ru");
     couchConfigProvider.setDB('gbook');
-    $httpProvider.defaults.withCredentials = true;
-
-    //couchConfigProvider.setMethod('method GET/JSONP');
-
-
   });
 	angular.module('schedulerApp')
   .controller('SchedulerCtrl', function ($scope, couchdb) {
     var $db = $scope.$db = couchdb;
     var _day;
-
-    // var _hot =hotRegisterer.getInstance('my-handsontable');
-    $scope.$on('authenticated', function(e,data) {
-      console.log("sheduler authenticated event data:", data);
-      $scope.authenticated=data;
-    });
 
     $scope._doc={
       grid: {
@@ -64,99 +49,41 @@ var $app = angular
 
         return td;
     };
-    $scope.dates=[];
 
-
-        $scope.location = function (_day) {
-          $location.path("/scheduler/"+ _day);
-          $scope.$apply
-        }
     $scope.settings = {
-      // contextMenu: [
-      //   'row_above', 'row_below', 'remove_row'
-      // ],
-      contextMenu: true,
+			readOnly: true,
       rowHeights: 50,
       stretchH: 'all',
       colWidths: 30, // can also be a number or a function
       rowHeaders: true,
       colHeaders: true,
-      //mergeCells: $scope._doc.grid.mergeCells,
-      // callbacks have 'on' prefix
-      onAfterInit: function() {
-        //console.log('onAfterInit call');
-      },
-      onAfterChange: function(index, amount) {
-        //console.log($scope._doc);
-        //console.log(index , amount);
-        if(index && $scope._doc._rev) {
-          if(!$scope._doc.cellStyle) $scope._doc.cellStyle=new Array(10);
-          if(!$scope._doc.cellStyle[index[0][0]]) $scope._doc.cellStyle[index[0][0]]={};
-          $scope._doc.cellStyle[index[0][0]][index[0][1]]="grey"
-          $db.doc.put($scope._doc, function(data) {
-            console.log("put:", data);
-            $scope._doc._rev=data.rev;
-          });
-        }
-
-
-      },
-      // contextMenuCopyPaste: {
-      //       swfPath: 'zeroclipboard/dist/ZeroClipboard.swf';
-      // }
-
     };
-
-
-    if($routeParams.day ===  undefined) {
-        var tomorrow=Date.today().add(1).days();
-        if(tomorrow.is().sunday()||tomorrow.is().saturday()) {
-          tomorrow=Date.today().next().monday();
-        }
-        $scope._day = moment(tomorrow).unix().toString();
-        $location.path("/scheduler/"+ $scope._day);
-      } else {
-        console.log($routeParams);
-        $scope._day = $routeParams.day;
-      }
-      moment.locale("en");
-    $scope._day_literal = moment.unix($scope._day).format("dddd").toLowerCase();
-
-    $db.doc.get($scope._day, function(data) {
-        $scope._doc=data;
-        console.log(data);
-    })
-      .error(function() {
-        //console.log("error");
-        $db.doc.get($scope._day_literal, function(data) {
-          $scope._doc=data;
-          moment.locale("ru");
-          $scope._doc.date=moment.unix($scope._day).format('dddd DD MMMM YYYY');
-          $scope._doc._id=$scope._day;
-          $scope._doc.type="schedule";
-          $scope._doc._rev=undefined;
-          console.log($db.user.get());
-          $scope._doc.author=$db.user.get().name;
-          $scope._doc.cellStyle=new Array(10);
-          // console.log(_hot.mergeCells.mergeRange(data.grid.mergeCells));
-          // _hot.mergeCells = new Handsontable.MergeCells(data.grid.mergeCells);
-          // _hot.updateSettings({ mergeCells: data.grid.mergeCells, cells: data.grid.data });
-          // _hot.render();
-          // $scope.settings.mergeCells = data.grid.mergeCells;
-          console.log($scope._doc);
-          //$scope.$apply();
-
-        });
-      });
-
-  $db.view('scheduler', 'schedules', {}, function(data) {
-    console.log(data);
-    $scope.dates=data.map(function(i) {
-      return i.id;
-    });
-    //$scope.$apply
-  });
+		var foo=1;
+		var _today=Date.today();
 
 
 
+		$scope._get=function(day) {
+			var _day = moment(day).unix().toString();
+			$db.doc.get(_day, function(data) {
+	        $scope._doc=data;
+	        console.log(data);
+	    })
+	      .error(function() {
+					foo++;
+					console.log("next day", day.add(1).day());
+					if(foo < 14) {
+						$scope._get(day.add(1).day())
+					}else if (foo==0) {
+						console.log("loading today");
+						return;
+					}else{
+						foo=0;
+						$scope._get(_today);
+					};
+	    });
+
+		}
+		$scope._get(_today.add(1).day());
+ 
   });
