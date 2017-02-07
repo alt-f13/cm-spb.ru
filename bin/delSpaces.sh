184c4de04a8baba74e6290622b718506abe8f6b9
Денис Галямин <galyamin.d.d@gmail.com>
2017-02-07 18:16:13 +0300

    образовательные программы

--- bin/delSpaces.sh
+++ bin/delSpaces.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+rm ~*
+rm Thumbs.*
+
+echo "converto PDF"
+soffice --headless --convert-to pdf "*.doc"
+
+rm *.doc
+rm *.docx
+
 find . -print0 | while read -d $'\0' i;
 do
  filename=$(basename "$i");
@@ -7,14 +16,19 @@ do
 	mv "${filename}" "${filename// /-}"
 done
 
+# find . -print0 | while read -d $'\0' i;
+# do
+# 	filename=$(basename "$i");
+# 	echo "<a href=${filename} class=\"list-group-item link-icon\">${filename//-/ }</a>"
+# done
+oldParent=""
 find . -print0 | while read -d $'\0' i;
 do
 	filename=$(basename "$i");
-	echo "<a href=${filename} class=\"list-group-item link-icon\">${filename//-/ }</a>"
-done
-
-find . -print0 | while read -d $'\0' i;
-do
-	filename=$(basename "$i");
+	parent=$(dirname "$i")
+	if [ "$parent" == "$oldParent" ]; then
+		echo "</div><h6>${parent//-/ }</h6><div class=\"list-group\">"
+	fi
 	echo "<a href=\"${i}\" class=\"list-group-item link-icon\">${filename//-/ }</a>"
+	oldParent=parent
 done
