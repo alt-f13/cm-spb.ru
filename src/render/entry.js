88d689b0b12b2e843d21dc0d140143e2131c4356
Денис Галямин <galyamin.d.d@gmail.com>
2017-02-15 11:17:00 +0300

    info affix header

--- src/render/entry.js
+++ src/render/entry.js
@@ -1,5 +1,6 @@
 ---
 webpack: true
+standalone: true
 ---
 
 /**
@@ -11,6 +12,10 @@ webpack: true
 // Example of how to load the package information with "json-loader":
 //var package = require("json!../package.json");
 
-//require('angular');
-//require('jquery')
+require('angular');
+require('jquery');
+
+require('angular-animate');
+require('angular-router');
+
 //require('../src/raw/bower_components/angular/angular.js');
