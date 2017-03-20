41e23026e48540fff836fd2f60accec792004d23
alt-f13 <galyamin.d.d@gmail.com>
2017-03-20 08:34:21 +0300

    webpack works

--- src/render/entry.js
+++ src/render/entry.js
@@ -1,7 +1,3 @@
----
-webpack: true
-standalone: true
----
 
 /**
  * Entry webpack file for plm-spb.ru.
@@ -10,12 +6,25 @@ standalone: true
  */
 
 // Example of how to load the package information with "json-loader":
-//var package = require("json!../package.json");
+//var package = require("json!../../package.json");
+
+var $ = require('jquery');
 
 require('angular');
-require('jquery');
-
-require('angular-animate');
-require('angular-router');
-
-//require('../src/raw/bower_components/angular/angular.js');
+require('bootstrap-loader');
+//require('font-awesome');
+//require('angular-animate');
+//require('angular-resource');
+require('lightbox2');
+require('lightbox2/dist/css/lightbox.css');
+//require('./css/index.css.scss')
+require('datejs');
+require('jquery.diamonds.js/src/diamonds.css');
+require('jquery.diamonds.js/src/jquery.diamonds.js');
+require('jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js');
+// require('');
+// require('');
+//require('../raw/bower_components/angular-simplecouch/angular.simplecouch.js');
+var vide = require('vide');
+  $('#programs').tubular({videoID: 't2my91byjR4'});
+  console.log('bootstrap function');
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,256 @@
+'use strict';
+
+// Modules
 var path = require("path");
-var webpack = require("webpack");
+
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/**
+ * Env
+ * Get npm lifecycle event to identify the environment
+ */
+var ENV = process.env.npm_lifecycle_event;
+var isTest = ENV === 'test' || ENV === 'test-watch';
+var isProd = ENV === 'build';
+
 module.exports = {
     resolve: {
-        modulesDirectories: ["/node_modules", "./src/raw/bower_components"]
+        modulesDirectories: ["web_modules", "node_modules", "bower_components"]
     },
-    plugins: [
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["dependencies"])
-        )
-    ]
+    // plugins: [
+    //     new webpack.ResolverPlugin(
+    //         new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+    //     )
+    // ]
 }
+
+
+module.exports = function makeWebpackConfig() {
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   * Should be an empty object if it's generating a test build
+   * Karma will set this when it's a test build
+   */
+  config.entry = isTest ? void 0 : {
+    app: './src/render/entry.js'
+  };
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   * Should be an empty object if it's generating a test build
+   * Karma will handle setting it up for you when it's a test build
+   */
+  config.output = isTest ? {} : {
+    // Absolute output directory
+    path: __dirname + '/out',
+
+    // Output path from the view of the page
+    // Uses webpack-dev-server in development
+    publicPath: isProd ? '/' : 'http://localhost:3000/',
+
+    // Filename for entry points
+    // Only adds hash in build mode
+    filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
+
+    // Filename for non-entry points
+    // Only adds hash in build mode
+    chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
+  };
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (isTest) {
+    config.devtool = 'inline-source-map';
+  }
+  else if (isProd) {
+    config.devtool = 'source-map';
+  }
+  else {
+    config.devtool = 'eval-source-map';
+  }
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+
+  // Initialize module
+  config.module = {
+    rules: [{
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    },
+    // {
+    //   // CSS LOADER
+    //   // Reference: https://github.com/webpack/css-loader
+    //   // Allow loading css through js
+    //   //
+    //   // Reference: https://github.com/postcss/postcss-loader
+    //   // Postprocess your css with PostCSS plugins
+    //   test: /\.css$/,
+    //   loader: isTest ? 'null-loader' : ExtractTextPlugin.extract({
+    //     fallbackLoader: 'style-loader',
+    //     loader: [
+    //       {loader: 'css-loader', query: {sourceMap: true}},
+    //       {loader: 'postcss-loader'}
+    //     ],
+    //   })
+    // },
+    {
+      test: /\.css$/,
+      use: [ 'style-loader', 'css-loader' ]
+    },{
+      test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery'
+    }, {
+      // ASSET LOADER
+      // Reference: https://github.com/webpack/file-loader
+      // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+      // Rename the file using the asset hash
+      // Pass along the updated reference to your code
+      // You can add here any file extension you want to get copied to your output
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+      loader: 'file-loader'
+    }, {
+      // HTML LOADER
+      // Reference: https://github.com/webpack/raw-loader
+      // Allow loading html through js
+      test: /\.html$/,
+      loader: 'raw-loader'
+    },{
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader"
+      }, {
+          loader: "css-loader"
+      }, {
+          loader: "sass-loader",
+          options: {
+              includePaths: ["/src/render/css/"]
+          }
+      }
+    ]
+  }]
+  };
+
+  // ISTANBUL LOADER
+  // https://github.com/deepsweet/istanbul-instrumenter-loader
+  // Instrument JS files with istanbul-lib-instrument for subsequent code coverage reporting
+  // Skips node_modules and files that end with .spec.js
+  if (isTest) {
+    config.module.rules.push({
+      enforce: 'pre',
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /\.spec\.js$/
+      ],
+      loader: 'istanbul-instrumenter-loader',
+      query: {
+        esModules: true
+      }
+    })
+  }
+
+  /**
+   * PostCSS
+   * Reference: https://github.com/postcss/autoprefixer-core
+   * Add vendor prefixes to your css
+   */
+   // NOTE: This is now handled in the `postcss.config.js`
+   //       webpack2 has some issues, making the config file necessary
+
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    new webpack.LoaderOptionsPlugin({
+      test: /\.scss$/i,
+      options: {
+        postcss: {
+          plugins: [autoprefixer]
+        }
+      }
+    }),
+    new webpack.ProvidePlugin({
+        $: "jquery/dist/jquery.min.js",
+        jQuery: "jquery/dist/jquery.min.js",
+        "window.jQuery": "jquery/dist/jquery.min.js"
+      })
+    // new webpack.ResolverPlugin(
+    //     new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+    // )
+  ];
+
+  //Skip rendering index.html in test mode
+  // if (!isTest) {
+  //   // Reference: https://github.com/ampedandwired/html-webpack-plugin
+  //   // Render index.html
+  //   config.plugins.push(
+  //     new HtmlWebpackPlugin({
+  //       template: './src/public/index.html',
+  //       inject: 'body'
+  //     }),
+  //
+  //     // Reference: https://github.com/webpack/extract-text-webpack-plugin
+  //     // Extract css files
+  //     // Disabled when in test mode or not in build mode
+  //     new ExtractTextPlugin({filename: 'css/[name].css', disable: !isProd, allChunks: true})
+  //   )
+  // }
+
+  // Add build specific plugins
+  if (isProd) {
+    config.plugins.push(
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      // Dedupe modules in the output
+      new webpack.optimize.DedupePlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      // Minify all javascript, switch loaders to minimizing mode
+      new webpack.optimize.UglifyJsPlugin(),
+
+      // Copy assets from the public folder
+      // Reference: https://github.com/kevlened/copy-webpack-plugin
+      new CopyWebpackPlugin([{
+        from: __dirname + '/src/public'
+      }])
+    )
+  }
+
+  /**
+   * Dev server configuration
+   * Reference: http://webpack.github.io/docs/configuration.html#devserver
+   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+   */
+  config.devServer = {
+    contentBase: './src/public',
+    stats: 'minimal'
+  };
+
+  return config;
+}();
